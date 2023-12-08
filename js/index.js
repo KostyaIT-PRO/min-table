b915c9525140946f90da2dd825a9c0eb3c147360
Konstantin Shcherba <120112867+KostyaIT-PRO@users.noreply.github.com>
2023-12-08 22:24:19 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const rows = Array.from(document.querySelectorAll('tr'));
+
+function slideOut(row){
+    row.classList.add('slide-out');
+}
+
+function slideIn(row, index){
+    setTimeout(function(){
+        row.classList.remove('slide-out');
+    }, (index + 5) * 300);
+}
+
+rows.forEach(slideOut);
+rows.forEach(slideIn);
